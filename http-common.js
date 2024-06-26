a20705bf6cd8939716c92c73cdeaa3418b222721
racheldenucci <denuccirachel@gmail.com>
2024-06-26 20:06:42 -0300

    fixed front end file upload

--- http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    //pode mudar dependendo da url da api rest
-  baseURL: "http://localhost:8080/api", 
-  headers: {
-    "Content-type": "application/json"
-  }
-});
